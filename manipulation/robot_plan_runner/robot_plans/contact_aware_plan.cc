9605a7aee5c4e7705956c001cf5f15f3db1fe73e
Pang Tao <pangtao@mit.edu>
2019-06-10 23:17:24 -0400

    add contact_aware_plan bael target

---
+++ manipulation/robot_plan_runner/robot_plans/contact_aware_plan.cc
@@ -0,0 +1,14 @@
+#include "contact_aware_plan.h"
+
+namespace drake {
+namespace manipulation {
+namespace robot_plan_runner {
+namespace robot_plans {
+
+
+
+}  // namespace robot_plans
+}  // namespace robot_plan_runner
+}  // namespace manipulation
+}  // namespace drake
+
---
+++ manipulation/robot_plan_runner/robot_plans/contact_aware_plan.h
@@ -0,0 +1,17 @@
+#pragma once
+
+namespace drake {
+namespace manipulation {
+namespace robot_plan_runner {
+namespace robot_plans {
+
+
+class contact_aware_plan {
+
+};
+
+
+}  // namespace robot_plans
+}  // namespace robot_plan_runner
+}  // namespace manipulation
+}  // namespace drake
