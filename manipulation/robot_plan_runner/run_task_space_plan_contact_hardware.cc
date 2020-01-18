42bf82f332c64afbe459e650d0598384245297bc
amazon <pangtao@mit.edu>
2020-01-17 21:38:36 -0500

    going-into-bin-and-out trajectory

--- manipulation/robot_plan_runner/run_task_space_plan_contact_hardware.cc
+++ manipulation/robot_plan_runner/run_task_space_plan_contact_hardware.cc
@@ -17,6 +17,35 @@ using std::endl;
 using std::vector;
 
 int run_plan() {
+  Eigen::VectorXd q0(7);
+  const double duration = 26;
+
+  //  // EE pushing against table.
+  //  q0 << -0.4532, 1.1335, -0.0479, -1.3982, -0.3034, 1.3395, -0.3055;
+  //  auto Q_WT = math::RollPitchYawd(0, M_PI * 1.25, 0).ToQuaternion();
+
+  //  // Link 6 making contact with table, the one in the 1st draft of the
+  //  paper. q0 << -0.965894, 1.41052, 0.548241, -1.46713, -0.973198, -1.49595,
+  //  -0.0991943; auto Q_WT = math::RollPitchYawd(0, M_PI / 3,
+  //  0).ToQuaternion(); Eigen::Vector3d t_knots1(0, duration / 2, duration);
+  //  Eigen::MatrixXd xyz_knots(3, 3);
+  //  xyz_knots.col(0) << 0, 0, 0;
+  //  xyz_knots.col(2) << 0, 0, -0.25;
+  //  xyz_knots.col(1) = (xyz_knots.col(0) + xyz_knots.col(2)) / 2;
+
+  // reaching into bin
+  q0 << -1.48595109, 1.25825224, 0.7347296, -1.60280037, -1.15394412,
+      0.77650079, -1.76714587;
+  auto Q_WT = math::RollPitchYawd(M_PI, 0, -M_PI / 2).ToQuaternion();
+
+  Eigen::VectorXd t_knots1(4);
+  t_knots1 << 0, 10, 13, 23;
+  Eigen::MatrixXd xyz_knots(3, 4);
+  xyz_knots.col(0) << 0, 0, 0;
+  xyz_knots.col(1) << 0, 0, -0.25;
+  xyz_knots.col(2) << 0, 0, -0.25;
+  xyz_knots.col(3) << 0, 0, 0;
+
   // create plan
   // plan0 goes to the starting pose of the contact-aware plan.
   PlanData plan0;
@@ -25,11 +54,7 @@ int run_plan() {
   t_knots0 << 0, 1;
 
   Eigen::MatrixXd q_knots(7, 2);
-//    q_knots.col(0) << -0.4532, 1.1335, -0.0479, -1.3982, -0.3034, 1.3395,
-//    -0.3055;  // EE pushing against the table.
-  q_knots.col(0) << -0.965894, 1.41052, 0.548241, -1.46713, -0.973198, -1.49595,
-      -0.0991943;  // Link 6 making contact with table.
-  q_knots.col(1) = q_knots.col(0);
+  q_knots.col(0) = q0;
 
   auto qtraj = trajectories::PiecewisePolynomial<double>::ZeroOrderHold(
       t_knots0, q_knots);
@@ -43,22 +68,12 @@ int run_plan() {
 
   PlanData::EeData ee_data;
   ee_data.p_ToQ_T << 0, 0, 0.10;
-
-  Eigen::Vector3d t_knots1(0, 8, 16);
-
-  Eigen::MatrixXd xyz_knots(3, 3);
-  xyz_knots.col(0) << 0, 0, 0;
-  xyz_knots.col(2) << 0, -0.22, -0.12;
-  xyz_knots.col(1) = (xyz_knots.col(0) + xyz_knots.col(2)) / 2;
-
-  ee_data.ee_xyz_traj = trajectories::PiecewisePolynomial<double>::Cubic(
-      t_knots1, xyz_knots, Eigen::VectorXd::Zero(3), Eigen::VectorXd::Zero(3));
+  ee_data.ee_xyz_traj =
+      trajectories::PiecewisePolynomial<double>::FirstOrderHold(t_knots1,
+                                                                xyz_knots);
   ee_data.ee_xyz_dot_traj = ee_data.ee_xyz_traj.derivative(1);
 
-//  auto Q_WT = math::RollPitchYawd(0, M_PI * 1.25, 0).ToQuaternion();
-  auto Q_WT = math::RollPitchYawd(0, M_PI / 3, 0).ToQuaternion();
-
-  vector<double> t_knots_v{t_knots1[0], t_knots1[1], t_knots1[2]};
+  vector<double> t_knots_v{0, duration / 2, duration};
   vector<Eigen::Quaterniond> quaternions{Q_WT, Q_WT, Q_WT};
   ee_data.ee_quat_traj =
       trajectories::PiecewiseQuaternionSlerp<double>(t_knots_v, quaternions);
