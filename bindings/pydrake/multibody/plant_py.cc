394b43031b2a0a1d295fc0905da47c5e539beb4b
Pang Tao <pangtao@mit.edu>
2020-12-22 23:17:53 -0500

    bind contact info

--- bindings/pydrake/multibody/plant_py.cc
+++ bindings/pydrake/multibody/plant_py.cc
@@ -128,7 +128,10 @@ void DoScalarDependentDefinitions(py::module m, T) {
         .def("num_hydroelastic_contacts", &Class::num_hydroelastic_contacts,
             cls_doc.num_hydroelastic_contacts.doc)
         .def("hydroelastic_contact_info", &Class::hydroelastic_contact_info,
-            py::arg("i"), cls_doc.hydroelastic_contact_info.doc);
+            py::arg("i"), cls_doc.hydroelastic_contact_info.doc)
+        .def("AddContactInfo",
+            py::overload_cast<const PointPairContactInfo<T>&>(
+                &Class::AddContactInfo));
     DefCopyAndDeepCopy(&cls);
     AddValueInstantiation<Class>(m);
   }
